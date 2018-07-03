417b42fa17c6728200df9502d9659d970f4d23ad
Thomas Dickerson <tadicke3@ncsu.edu>
2018-07-02 22:33:02 -0400

    Added minor tweaks to ChatBot IO, and licensing information.

--- IsThisLossBot.py
+++ IsThisLossBot.py
@@ -1,3 +1,15 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import requests
 import IsThisLossHelper as helper
 import IsThisLossBotConfig as conf
@@ -26,59 +38,56 @@ response = response = requests.get('https://api.groupme.com/v3/groups/' + conf.g
 
 print("Ready to run!")
 
+intro_text = ("Hey there, my name is IsThisLossBotV1!\n"
+              "I\'ll be sure to butt into the conversation if I see a Loss meme!\n"
+              "Today I am " + str(100*accuracy) + "% accurate!")
+post_params = { 'bot_id' : conf.bot_id(), 'text': intro_text }
+requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
+
 while True:
     response = requests.get('https://api.groupme.com/v3/groups/' + conf.group_id() + '/messages', params = request_params)
     if response.status_code == 200:
         response_messages = response.json()['response']['messages']
 
         for message in response_messages:
-            pics = 0
-            if ("@IsThisLossBot" in message['text']):
-                for attachment in message['attachments']:
-                    if (attachment['type'] == "image"):
-                        print(attachment['url'])
-                        pic_response = requests.get(attachment['url'])
-                        img = Image.open(BytesIO(pic_response.content))
-                        # Resize image to 100x100
-                        final_size = 100
-                        new_image_size = (final_size, final_size)
-                        img = img.resize(new_image_size, Image.ANTIALIAS)
-                        new_im = Image.new("RGB", (final_size, final_size))
-                        new_im.paste(img, (0, 0))
-                        img = new_im
-                        # Make image grayscale
-                        img = img.convert('LA')
-                        # Create array of image info
-                        pic_array = [[]]
-                        width, height = img.size
-                        data = img.load()
-                        pic_array[0].append(0)
-                        for y in range(height):
-                            for x in range(width):
-                                pic_array[0].append(data[x,y][0])
-                        df = pd.DataFrame(data=pic_array)
-                        prediction_targets, prediction_examples = helper.parse_labels_and_features(df)
-                        predict_input_fn = helper.create_predict_input_fn(
-                          prediction_examples, prediction_targets, 1)
-                        predictions = list(classifier.predict(input_fn=predict_input_fn))
-                        predicted_classes = [p["classes"] for p in predictions]
-                        print(predictions)
-                        for c in predicted_classes:
-                            predicted_class = str(c[0])
-                            if predicted_class == "b\'1\'":
-                                text = "Is this loss?"
-                                print(predicted_class)
-                            else:
-                                text = "Hmmm..."
-                                print(predicted_class)
-                            post_params = { 'bot_id' : conf.bot_id(), 'text': text }
-                            requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
-                        pics += 1
-                if pics == 0:
-                    text = "There aren't any pictures in your message..."
-                    post_params = { 'bot_id' : conf.bot_id(), 'text': text }
-                    requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
+            for attachment in message['attachments']:
+                if (attachment['type'] == "image"):
+                    print(attachment['url'])
+                    pic_response = requests.get(attachment['url'])
+                    img = Image.open(BytesIO(pic_response.content))
+                    # Resize image to 100x100
+                    final_size = 100
+                    new_image_size = (final_size, final_size)
+                    img = img.resize(new_image_size, Image.ANTIALIAS)
+                    new_im = Image.new("RGB", (final_size, final_size))
+                    new_im.paste(img, (0, 0))
+                    img = new_im
+                    # Make image grayscale
+                    img = img.convert('LA')
+                    # Create array of image info
+                    pic_array = [[]]
+                    width, height = img.size
+                    data = img.load()
+                    pic_array[0].append(0)
+                    for y in range(height):
+                        for x in range(width):
+                            pic_array[0].append(data[x,y][0])
+                    df = pd.DataFrame(data=pic_array)
+                    prediction_targets, prediction_examples = helper.parse_labels_and_features(df)
+                    predict_input_fn = helper.create_predict_input_fn(
+                      prediction_examples, prediction_targets, 1)
+                    predictions = list(classifier.predict(input_fn=predict_input_fn))
+                    predicted_classes = [p["classes"] for p in predictions]
+                    print(predictions)
+                    for c in predicted_classes:
+                        predicted_class = str(c[0])
+                        if predicted_class == "b\'1\'":
+                            text = "Is this loss?"
+                            print(predicted_class)
+                        else:
+                            print(predicted_class)
+                        post_params = { 'bot_id' : conf.bot_id(), 'text': text }
+                        requests.post('https://api.groupme.com/v3/bots/post', params = post_params)
             request_params['after_id'] = message['id']
-
 
     time.sleep(1)
---
+++ README.md
@@ -0,0 +1,21 @@
+# Licensing
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+# Special Thanks
+
+Images ripped from /r/lossedits using [ripme.](https://github.com/4pr0n/ripme)
+
+Images resized using [script by John Ottenlips.](https://stackoverflow.com/questions/21517879/python-pil-resize-all-images-in-a-folder)
+
+CSV Files generated with reference from [James Kellas.](https://www.quora.com/How-can-my-pixel-data-from-an-image-be-outputted-into-a-CSV-file-like-this-in-Python)
+
+Machine Learning Python code created with [reference from Google's crash course.](http://developers.google.com/machine-learning/crash-course/)
+
+GroupMe bot made with inspiration from Stephen Waddell.
+
+GroupMe bot made with reference to [tutorial.](http://sweb.uky.edu/~jtba252/index.php/2017/09/13/how-to-write-a-groupme-bot-using-python/)
